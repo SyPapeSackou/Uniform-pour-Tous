543aff84413f24438013c08862b08f0a6f67259a
SyPapeSackou <papesackousy@gmail.com>
2025-12-20 15:08:27 +0100

    Fix: Force l'ajout du dossier vendor pour Vercel

--- api/index.php
+++ api/index.php
@@ -1,14 +1,14 @@
 <?php
-// Force l'affichage des erreurs sur la page
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// // Force l'affichage des erreurs sur la page
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-// Vérification de l'existence du fichier avant le require
-$laravelIndex = __DIR__ . '/../public/index.php';
+// // Vérification de l'existence du fichier avant le require
+// $laravelIndex = __DIR__ . '/../public/index.php';
 
-if (!file_exists($laravelIndex)) {
-    die("Erreur : Le fichier public/index.php est introuvable au chemin : " . $laravelIndex);
-}
-
-require $laravelIndex;
+// if (!file_exists($laravelIndex)) {
+//     die("Erreur : Le fichier public/index.php est introuvable au chemin : " . $laravelIndex);
+// }
+echo "PHP fonctionne. Version : " . phpversion();
+// require $laravelIndex;
