7cb0821c258791ef699a97674eb73e67b4fd5d26
SyPapeSackou <papesackousy@gmail.com>
2025-12-20 15:17:53 +0100

    Fix: Force l'ajout du dossier vendor pour Vercel

--- api/index.php
+++ api/index.php
@@ -1,14 +1,4 @@
 <?php
-// 1. On affiche les erreurs pour voir le coupable
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//require __DIR__ . '/../public/index.php';
+echo "PHP fonctionne. Version : " . phpversion();
 
-// 2. Chemin vers Laravel
-$laravel = __DIR__ . '/../public/index.php';
-
-if (!file_exists($laravel)) {
-    die("Erreur : Le dossier 'public' ou le fichier 'index.php' est introuvable sur le serveur.");
-}
-
-// 3. On lance Laravel
-require $laravel;
