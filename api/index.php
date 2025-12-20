ccc521523fe3ba2b206292bf0ccb498bdbcff7b7
SyPapeSackou <papesackousy@gmail.com>
2025-12-20 15:19:05 +0100

    Fix: Force l'ajout du dossier vendor pour Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
-//require __DIR__ . '/../public/index.php';
-echo "PHP fonctionne. Version : " . phpversion();
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Exception $e) {
+    echo "Erreur Laravel : " . $e->getMessage();
+} catch (\Throwable $t) {
+    echo "Erreur Critique : " . $t->getMessage();
+}
